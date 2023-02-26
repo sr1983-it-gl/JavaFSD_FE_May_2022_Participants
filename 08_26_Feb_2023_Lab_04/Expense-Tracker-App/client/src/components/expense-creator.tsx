6754f3c15f1156b7bc26db8b9c5ded1a0e33d862
sr1983-it-edureka <sr1983.it.edureka@gmail.com>
2023-02-26 13:47:15 +0530

    Add Expense Item - Part II - UI Changes

--- 08_26_Feb_2023_Lab_04/Expense-Tracker-App/client/src/components/expense-creator.tsx
+++ 08_26_Feb_2023_Lab_04/Expense-Tracker-App/client/src/components/expense-creator.tsx
@@ -1,5 +1,5 @@
 
-import {Button, Modal} from "react-bootstrap"
+import {Button, Modal, Form} from "react-bootstrap"
 
 import {useState} from "react"
 
@@ -19,20 +19,49 @@ const ExpenseCreator = () => {
     <Modal show={show} onHide={handleClose}>
       
       <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
+        <Modal.Title>Add Expense Item</Modal.Title>
       </Modal.Header>
 
-      <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+      <Modal.Body>
 
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={handleClose}>
-          Save Changes
-        </Button>
-      </Modal.Footer>
+          <Form>
+            
+            <Form.Group className="mb-3" controlId="expenseDescription">
+              <Form.Label>Expense Description</Form.Label>
+              <Form.Control type="text" placeholder="Enter expense description" />
+            </Form.Group>
 
+            <Form.Group className="mb-3" controlId="payeeName">
+              <Form.Label>Payee</Form.Label>
+
+                <Form.Select aria-label="Default select example">
+                  <option>Open this select menu</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </Form.Select>
+
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="price">
+              <Form.Label>price</Form.Label>
+              <Form.Control type="number" placeholder="Enter price" />
+            </Form.Group>            
+
+            <Form.Group className="mb-3" controlId="expenseDate">
+              <Form.Label>Expense Date</Form.Label>
+              <Form.Control type="date" />
+            </Form.Group>            
+
+            <Button variant="primary" type="submit">
+              Add Expense Item
+            </Button>
+
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Form>
+      </Modal.Body>
 
     </Modal>    
 
